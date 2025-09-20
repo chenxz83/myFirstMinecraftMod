e52cfa7e5c6be3edbed15f6423eb1dc2c8fb4a3a
chenxz <chenxz6611@outlook.com>
2025-09-20 11:23:04 +0800

    Initial commit

---
+++ src/main/java/com/besson/tutorial/TutorialModClient.java
@@ -0,0 +1,10 @@
+package com.besson.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
